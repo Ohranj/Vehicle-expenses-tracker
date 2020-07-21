b27588de70c9bc6cafce48725b6120061de97c06
Ohranj <ajdorrington@hotmail.co.uk>
2020-07-21 14:30:56 +0100

    15th commit - Added edit feature to vehicles

--- client/README.md
+++ client/README.md
@@ -1,8 +1,8 @@
-Add loading feature when return to page after delete
-Reformat dates on expenses table
 Add a total cost to expenses table - display on vehicle table?
 
 
 existing demo account:
+
 username - demo1@demo1.com 
+
 password - demo1
--- client/src/components/dash/RenderExpenses.js
+++ client/src/components/dash/RenderExpenses.js
@@ -3,18 +3,23 @@ import React from 'react'
 
 class RenderExpenses extends React.Component {
 
+
     render() {
         if (this.props.expense.length === 0) {
              return null   
         } else {
             return (
                 this.props.expense.map((expense) => {
+                    const year = expense.date.substring(0, 4)
+                    const month = expense.date.substring(5, 7)
+                    const day = expense.date.substring(8, 10)
+                    const expenseDate = `${day}/${month}/${year}`
                     return (
                         <tr key={expense._id}>
                             <td>{expense.name}</td>
                             <td>{expense.description}</td>
                             <td>{expense.value}</td>
-                            <td>{expense.date}</td>
+                            <td>{expenseDate}</td>
                             <td>
                                 <button
                                     className="ui red button tableBtn"
